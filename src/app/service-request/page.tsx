ee15be0e3c238d986a496bfea4e85e9990ecea57
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-10-15 01:38:49 +0800

    [Myk][Add] Add left tab

---
+++ src/app/service-request/page.tsx
@@ -0,0 +1,12 @@
+import LeftTab from "@/components/layouts/LeftTab"
+
+export default function Page() {
+  return (
+    <div className="w-screen h-screen flex">
+      <LeftTab />
+      <div className="w-full h-full">
+        
+      </div>
+    </div>
+  )
+}
