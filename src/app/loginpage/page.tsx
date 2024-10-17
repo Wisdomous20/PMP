7129e6d1d63be72b2a18bb3a394de01871e2e087
Wisdomous20 <aljason.javier-20@cpu.edu.ph>
2024-10-17 21:05:50 +0800

    [Aljason][add]supabase set up for next auth and middleware

--- src/app/loginpage/page.tsx
+++ src/app/loginpage/page.tsx
@@ -1,9 +1,14 @@
-import LoginPage from "@/components/ui/loginpage/LoginPage";
+import { login, signup } from './action'
 
-export default function Login() {
+export default function LoginPage() {
   return (
-    <div className="">
-      <LoginPage />
-    </div>
-  );
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+      <button formAction={signup}>Sign up</button>
+    </form>
+  )
 }
