8bf0e578e27c2103d321c434ab6b7f5a898f8089
sheriiiiii <shereelaluma1024@gmail.com>
2024-10-18 15:27:52 +0800

    [She][Fix] Fix build errors by removing unused components in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import LeftTab from "@/components/layouts/LeftTab"
-import ServiceRequestList from "@/components/view-service-request/ServiceRequestList"
-import { Card, CardHeader } from "@/components/ui/card"
-import { Separator } from "@/components/ui/separator"
 import CreateServiceRequest from "@/components/create-service-request/CreateServiceRequest"
 
 export default function Page() {
