66b7af4ec6c6eb824146c63cabd2fbddf357f046
Carlow18 <124373867+Carlow18@users.noreply.github.com>
2024-10-18 14:29:52 +0800

    [Carlo][chore] fixed folder names for login and register page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,4 @@
-import LoginPage from "@/components/ui/loginpage/LoginPage";
-
+import LoginPage from "@/components/auth/LoginPage";
 export default function Login() {
   return (
     <div className="">
