3b3aae8669b982e3f3d65119eedd7f25eb35bf3b
sheriiiiii <shereelaluma1024@gmail.com>
2024-10-18 15:24:11 +0800

    [She][Add] added create service request card on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+import LeftTab from "@/components/layouts/LeftTab"
+import ServiceRequestList from "@/components/view-service-request/ServiceRequestList"
+import { Card, CardHeader } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+import CreateServiceRequest from "@/components/create-service-request/CreateServiceRequest"
 
+export default function Page() {
+  return (
+    <div className="w-screen h-screen flex">
+      <LeftTab />
+      <CreateServiceRequest />
     </div>
-  );
+  )
 }
