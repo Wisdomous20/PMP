613ac56abd24f2ffbf68351b63a3d2bdf4df529b
Thel-nny <124367376+Thel-nny@users.noreply.github.com>
2024-10-22 18:09:22 +0800

    [Thel] [Test] Unit test for Register

--- __tests__/register/register.test.tsx
+++ __tests__/register/register.test.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import "@testing-library/jest-dom/extend-expect";
-import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import Register from "@/components/auth/Register";
 import { supabase } from "@/lib/supabaseClient";
+import { Target } from "lucide-react";
 
 jest.mock('@/lib/supabaseClient', () => ({
     supabase:{
@@ -27,6 +28,29 @@ describe("Register Component", () => {
         expect(screen.getByLabelText(/Confirm Password/i)).toBeInTheDocument();
         expect(screen.getByRole("button", { name: /Register/i })).toBeInTheDocument();
     });
+
+    it("displays error messages for invalid inputs", async () =>{
+        render(<Register />);
+
+        fireEvent.click(screen.getByRole("button", {name:  /Register/i}));
+        await waitFor(() => {
+            expect(screen.getByText(/Username must be at least 3 characters/i)).toBeInTheDocument();
+            expect(screen.getByText(/Please enter a valid CPU email address/i)).toBeInTheDocument();
+            expect(screen.getByText(/Please enter a valid CPU email address/i)).toBeInTheDocument();
+            expect(screen.getByText(/Password must be at least 8 characters long, contain one uppercase letter, one lowercase letter, one number, and one special character./i)).toBeInTheDocument();  
+        })
+    })
+
+    it("validates email format", async ()=> {
+        render(<Register />);
+
+        fireEvent.change(screen.getByLabelText(/Email/i), {Target: {value: "invalid-email"}});
+        fireEvent.click(screen.getByRole("button", {name:  /Register/i}));
+        await waitFor(() => {
+            expect(screen.getByText(/Please enter a valid CPU email address/i)).toBeInTheDocument();
+        })
+    })
+
 });
 
 
