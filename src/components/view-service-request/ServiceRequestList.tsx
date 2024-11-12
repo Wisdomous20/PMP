5bcf0e4f57bd31b3e853229e7d2c91bdc103528c
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-11-12 15:07:05 +0800

    [Myk][Refactor] - Refactor get user session to be server-side by creating hook, created middleware for protected routes

--- src/components/view-service-request/ServiceRequestList.tsx
+++ src/components/view-service-request/ServiceRequestList.tsx
@@ -1,41 +1,12 @@
 "use client"
 import ServiceRequestPreview from "./ServiceRequestPreview"
 import getServiceRequestFetch from "@/utils/service-request/getServiceRequestFetch"
-import { useSession } from "next-auth/react"
-import { redirect } from "next/navigation";
+import useGetSessionData from "@/hooks/useGetSessionData";
 import { useState, useEffect } from "react";
 
-// const mockRequests: ServiceRequest[] = [
-//   { requesterName: 'John Doe', title: 'New Equipment', details: 'Requesting a new laptop for development work', createdOn: '2023-10-15' },
-//   { requesterName: 'Jane Smith', title: 'Software License', details: 'Need a license for Adobe Creative Suite', createdOn: '2023-10-14' },
-//   { requesterName: 'Bob Johnson', title: 'Training Course', details: 'Approval for attending a React conference', createdOn: '2023-10-13' },
-//   { requesterName: 'Alice Brown', title: 'Office Supplies', details: 'Ordering new ergonomic chairs for the team', createdOn: '2023-10-12' },
-//   { requesterName: 'Charlie Wilson', title: 'Travel Approval', details: 'Business trip to New York for client meeting', createdOn: '2023-10-11' },
-//   { requesterName: 'John Doe', title: 'New Equipment', details: 'Requesting a new laptop for development work', createdOn: '2023-10-15' },
-//   { requesterName: 'Jane Smith', title: 'Software License', details: 'Need a license for Adobe Creative Suite', createdOn: '2023-10-14' },
-//   { requesterName: 'Bob Johnson', title: 'Training Course', details: 'Approval for attending a React conference', createdOn: '2023-10-13' },
-//   { requesterName: 'Alice Brown', title: 'Office Supplies', details: 'Ordering new ergonomic chairs for the team', createdOn: '2023-10-12' },
-//   { requesterName: 'Charlie Wilson', title: 'Travel Approval', details: 'Business trip to New York for client meeting', createdOn: '2023-10-11' },
-//   { requesterName: 'John Doe', title: 'New Equipment', details: 'Requesting a new laptop for development work', createdOn: '2023-10-15' },
-//   { requesterName: 'Jane Smith', title: 'Software License', details: 'Need a license for Adobe Creative Suite', createdOn: '2023-10-14' },
-//   { requesterName: 'Bob Johnson', title: 'Training Course', details: 'Approval for attending a React conference', createdOn: '2023-10-13' },
-//   { requesterName: 'Alice Brown', title: 'Office Supplies', details: 'Ordering new ergonomic chairs for the team', createdOn: '2023-10-12' },
-//   { requesterName: 'Charlie Wilson', title: 'Travel Approval', details: 'Business trip to New York for client meeting', createdOn: '2023-10-11' },
-//   { requesterName: 'John Doe', title: 'New Equipment', details: 'Requesting a new laptop for development work', createdOn: '2023-10-15' },
-//   { requesterName: 'Jane Smith', title: 'Software License', details: 'Need a license for Adobe Creative Suite', createdOn: '2023-10-14' },
-//   { requesterName: 'Bob Johnson', title: 'Training Course', details: 'Approval for attending a React conference', createdOn: '2023-10-13' },
-//   { requesterName: 'Alice Brown', title: 'Office Supplies', details: 'Ordering new ergonomic chairs for the team', createdOn: '2023-10-12' },
-//   { requesterName: 'Charlie Wilson', title: 'Travel Approval', details: 'Business trip to New York for client meeting', createdOn: '2023-10-11' },
-//   { requesterName: 'John Doe', title: 'New Equipment', details: 'Requesting a new laptop for development work', createdOn: '2023-10-15' },
-//   { requesterName: 'Jane Smith', title: 'Software License', details: 'Need a license for Adobe Creative Suite', createdOn: '2023-10-14' },
-//   { requesterName: 'Bob Johnson', title: 'Training Course', details: 'Approval for attending a React conference', createdOn: '2023-10-13' },
-//   { requesterName: 'Alice Brown', title: 'Office Supplies', details: 'Ordering new ergonomic chairs for the team', createdOn: '2023-10-12' },
-//   { requesterName: 'Charlie Wilson', title: 'Travel Approval', details: 'Business trip to New York for client meeting', createdOn: '2023-10-11' },
-// ]
-
 export default function ServiceRequestList() {
-  const [ServiceRequests, setServiceRequests] = useState<ServiceRequest[]>([])
-  const { data: session } = useSession()
+  const [ ServiceRequests, setServiceRequests ] = useState<ServiceRequest[]>([])
+  const { sessionData: session } = useGetSessionData()
 
   useEffect(() => {
     const fetchServiceRequests = async () => {
@@ -49,10 +20,6 @@ export default function ServiceRequestList() {
 
     fetchServiceRequests()
   }, [])
-
-  if (!session) {
-    return redirect("/auth/login");
-  }
 
   return (
     <div className="w-full h-full flex flex-col overflow-y-scroll overflow-x-hidden space-y-2">
