78602f3d07e9f04112fb5f4639fdda736104d7e0
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-11-20 10:44:45 +0800

    [Refactor] Small UI changes to service request list, details, and preview

--- src/components/view-service-request/ServiceRequestPreviewShe.tsx
+++ src/components/view-service-request/ServiceRequestPreviewShe.tsx
@@ -11,8 +11,8 @@ interface ServiceRequestPreviewProps {
   createdOn: string;
 }
 
-export default function ServiceRequestPreviewShe({ id, requesterName, title, details, createdOn}: ServiceRequestPreviewProps) {
-  const detailsPreview = details.length > 50? details.slice(0,50) + '...' : details;
+export default function ServiceRequestPreviewShe({ id, requesterName, title, details, createdOn }: ServiceRequestPreviewProps) {
+  const detailsPreview = details.length > 50 ? details.slice(0, 50) + '...' : details;
   return (
 
     // <Button 
@@ -34,24 +34,27 @@ export default function ServiceRequestPreviewShe({ id, requesterName, title, det
     //       Normal
     //     </Button>
     <Link href={`/service-request/${id}`}>
-    <Card key={id}>
-      <CardContent className="flex justify-between items-center p-4">
-        <div className="pb-2">
-          <h2 className="pb-2 text-md font-semibold">{requesterName}</h2>
-          <p className="pb-1text-sm text-gray-semibold">{title}</p>
-          <p className="text-sm text-muted-foreground">{detailsPreview}</p>
-        </div>
-        <span className="text-sm text-gray-500">{formatTimestamp(createdOn)}</span>
-      </CardContent>
-      <div className="m-3 flex justify-start space-x-4">
+      <Card key={id} className="rounded-md">
+        <CardContent className="flex justify-between items-center p-4">
+          <div className="w-full">
+            <div className="flex flex-row justify-between items-center">
+              <h2 className="pb-1 font-semibold">{requesterName}</h2>
+              <span className="text-sm text-gray-500">{formatTimestamp(createdOn)}</span>
+            </div>
+
+            <p className="text-sm text-gray-semibold">{title}</p>
+            <p className="text-sm text-muted-foreground">{detailsPreview}</p>
+          </div>
+        </CardContent>
+        {/* <div className="m-3 flex justify-start space-x-4">
           <button className="px-3 py-3 bg-gray-500 text-white text-xs rounded-md">
             Reject Request
           </button>
           <button className="px-3 py-3 bg-gray-500 text-white text-xs rounded-md">
             Approve Request
           </button>
-        </div>
-    </Card>
-  </Link>
-);
+        </div> */}
+      </Card>
+    </Link>
+  );
 }
