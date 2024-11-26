2c987ed944521b34f219f80ec8f67cf9e09f9f32
Thel-nny <124367376+Thel-nny@users.noreply.github.com>
2024-11-27 00:18:18 +0800

    [Style][Thel] Changed LeftTab UI

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
+        gold: "group hover:bg-amber-400 text-gray-200 hover:text-indigo-800",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
