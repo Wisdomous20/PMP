228423e1942f960e5e6b6bbf4569adb165087928
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-12-08 18:12:34 +0800

    [Myk][Add] Add projects tab for admin and supervisors

--- src/components/layouts/LeftTab.tsx
+++ src/components/layouts/LeftTab.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { Plus, FileText, Archive, LogOut } from "lucide-react";
+import { Plus, FileText, Archive, LogOut, Folder } from "lucide-react";
 import Link from "next/link";
 import useGetUserRole from "@/domains/user-management/hooks/useGetUserRole";
-import LoadingSpinner from "@/components/ui/loadingDots"
+import LoadingSpinner from "@/components/ui/loadingDots";
 
 export default function LeftTab() {
   const { userRole, loading } = useGetUserRole();
@@ -27,11 +27,21 @@ export default function LeftTab() {
         />
         <span className="sr-only">CPU Logo</span>
       </Button>
+      
       {userRole === "USER" && (
         <Link href="/service-request/create">
           <Button variant="gold" size="icon" className="w-11 h-12">
             <Plus className="w-7 h-7" />
             <span className="sr-only">Create Service Request</span>
+          </Button>
+        </Link>
+      )}
+
+      {(userRole === "SUPERVISOR" || userRole === "ADMIN") && (
+        <Link href="/projects">
+          <Button variant="gold" size="icon" className="w-11 h-12">
+            <Folder className="w-6 h-6" />
+            <span className="sr-only">Projects</span>
           </Button>
         </Link>
       )}
@@ -42,12 +52,14 @@ export default function LeftTab() {
           <span className="sr-only">Service Request List</span>
         </Button>
       </Link>
+      
       <Link href="/service-request/archive">
         <Button variant="gold" size="icon" className="w-11 h-12">
           <Archive className="w-6 h-6" />
           <span className="sr-only">Archive</span>
         </Button>
       </Link>
+      
       <Link href="/auth/login" onClick={handleLogout}>
         <Button variant="gold" size="icon" className="w-11 h-12">
           <LogOut className="w-6 h-6" />
