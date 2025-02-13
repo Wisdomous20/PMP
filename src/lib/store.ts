2efbb99d7c9e0fce21258964bf7fb5c6591c0c24
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2025-02-13 16:07:47 +0800

    [Add] Initial redux implementation

---
+++ src/lib/store.ts
@@ -0,0 +1,16 @@
+// lib/store.ts
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './features/counter/counterSlice'
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      counter: counterReducer,
+    },
+    devTools: process.env.NODE_ENV !== 'production',
+  })
+}
+
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
