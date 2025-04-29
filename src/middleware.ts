5950bcf23c35e35aafcb5c073fcd0f41652f8e05
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2025-04-29 19:05:50 +0800

    [Myk][Add] Landing page

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,12 @@ export { default } from "next-auth/middleware"
 
 export const config = {
   matcher: [
-    "/",
+    // "/",
     "/service-request/:path*",
+    "/projects/:path",
+    "/personnel-management/:path",
+    "/inventory-management/:path",
+    "/implementation-plan/:path",
+    "/dashboard/:path"
   ],
 }
