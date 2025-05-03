019edc711432edec634f0c3df0c048e5fcc5ad29
Thel-nny <124367376+Thel-nny@users.noreply.github.com>
2025-05-03 14:40:56 +0800

    [THEL][STYLE] chaned the EditImplementationPlan button

--- src/components/implementation-plan/ImplementationPlanPreview.tsx
+++ src/components/implementation-plan/ImplementationPlanPreview.tsx
@@ -2,10 +2,7 @@
 import React from "react";
 import EditImplementationPlan from "./EditImplementationPlanMyk";
 import { Card, CardHeader, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
-import { MoreVertical } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import useGetUserRole from "@/domains/user-management/hooks/useGetUserRole";
 interface ImplementationPlanPreviewProps {
   plan: ImplementationPlan;
@@ -32,24 +29,16 @@ const ImplementationPlanPreview: React.FC<ImplementationPlanPreviewProps> = ({ p
 
   return (
     <Card key={plan.id} className="border border-gray-100 shadow-none">
-      <CardHeader className="flex items-start justify-between p-3 pb-0">
-        <div>
+      <CardHeader className="flex flex-col p-3 pb-0">
+        <div className="flex justify-between items-center">
           <h3 className="font-medium text-gray-700 truncate">{sr?.concern || "No Concern"}</h3>
-          <p className="text-xs text-gray-500 truncate">{sr?.details || "No Details"}</p>
-        </div>
-        {!isSecretary && (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size="icon" className="h-7 w-7">
-                <MoreVertical className="h-4 w-4" />
-                <span className="sr-only">Open menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
+          {!isSecretary && (
+            <div className="relative -top-1">
               <EditImplementationPlan serviceRequest={sr} tasksInitial={tasksInitial} />
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
+            </div>
+          )}
+        </div>
+        <p className="text-xs text-gray-500 truncate">{sr?.details || "No Details"}</p>
       </CardHeader>
       <CardContent className="p-3 pt-2">
         <div className="text-xs text-gray-500 mb-1.5 flex justify-between">
