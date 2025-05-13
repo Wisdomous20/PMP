09a8a12660de2b2c46ff3c86fc3d18ca06a41d75
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2025-05-14 06:38:36 +0800

    [Myk][Add] Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import React from "react";
+import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import ClientProvider from "./ClientProvider";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "SIGRA8",
+  description: "Sigra8 Film Festival",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
