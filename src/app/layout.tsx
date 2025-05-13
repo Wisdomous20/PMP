a70b492d58b621373582c1d1a1be8b0cb50dde42
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2025-05-14 06:58:07 +0800

    [Myk][Fix] Build issue caused by use client in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,20 @@
-"use client";
-
 import React from "react";
 import type { Metadata } from "next";
-import { SessionProvider } from "next-auth/react";
 import ClientProvider from "./ClientProvider";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SIGRA8",
-  description: "Sigra8 Film Festival",
+  title: "Project Management System",
+  description: "CPU Project Management System",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider>
-          <ClientProvider>
-            {children}
-          </ClientProvider>
-        </SessionProvider>
+        <ClientProvider>
+          {children}
+        </ClientProvider>
       </body>
     </html>
   );
