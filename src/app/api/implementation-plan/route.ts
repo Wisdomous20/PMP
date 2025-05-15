4d0f9d7a901ee5bc43dbbab5a319c14d598e1c3d
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2025-05-16 06:59:36 +0800

    [Myk][Fix] Notifications

--- src/app/api/implementation-plan/route.ts
+++ src/app/api/implementation-plan/route.ts
@@ -1,17 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import CreateImplementationPlan from "@/domains/implementation-plan/services/CreateImplementationPlan";
+import { createNotification } from "@/domains/notification/services/createNotification";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
-    const {serviceRequestId, tasks} = await req.json();
+    const { serviceRequestId, tasks } = await req.json();
     if (!tasks) {
       return NextResponse.json(
         { error: "Missing required fields" },
         { status: 400 }
       );
     }
-    const newImplementationPlan = await CreateImplementationPlan(serviceRequestId, tasks);
-    
+    const newImplementationPlan = await CreateImplementationPlan(
+      serviceRequestId,
+      tasks
+    );
+    await createNotification(
+      "implementation_plan",
+      `An implementation plan was created for the Service Request ${newImplementationPlan.serviceRequest.concern}.`,
+      `/service-requests/${serviceRequestId}`
+    );
+
     return NextResponse.json(newImplementationPlan, { status: 201 });
   } catch (error) {
     console.error("Error creating implementation plan:", error);
