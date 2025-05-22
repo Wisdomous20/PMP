10e7430c25d5086607cac63746958576f04f2eb2
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2025-05-23 06:07:55 +0800

    [Myk][Fix] Possibly fix supervisors not all showing by removing chache

--- src/app/api/supervisor/route.ts
+++ src/app/api/supervisor/route.ts
@@ -1,11 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextRequest, NextResponse } from "next/server";
 import getSupervisors from "@/domains/user-management/services/getSupervisors";
 
+export const dynamic = "force-dynamic";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(req: NextRequest): Promise<NextResponse> {
   try {
     const supervisors = await getSupervisors();
-    return NextResponse.json(supervisors, { status: 200 });
+    return NextResponse.json(supervisors, {
+      status: 200,
+      headers: {
+        "Cache-Control": "no-store, max-age=0",
+      },
+    });
   } catch (error) {
     console.error("Error fetching supervisors:", error);
     return NextResponse.json(
