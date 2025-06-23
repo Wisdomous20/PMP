33f1514ced54389830673595f9bea2a37454489a
ohana-matsumae <76694828+ohana-matsumae@users.noreply.github.com>
2025-06-23 19:58:26 +0900

    chore: cleanup and lint fixes for build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
+    darkMode: 'class',
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -77,6 +77,7 @@ const config: Config = {
   		},
   	},
   },
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("tailwindcss-animate")],
 };
 export default config;
